1aabc119a409c6bc56f8edb53a4373aa306ad75f
Anton Ruvinsky <railanmoon@gmail.com>
2022-06-09 20:13:19 +0300

    Fixed path to icon

--- JsonToCsvHomeWork/ClientApp/src/components/NavMenu.js
+++ JsonToCsvHomeWork/ClientApp/src/components/NavMenu.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
-import JsonConverterIcon from '/icons/JsonConverter.png';
+import JsonConverterIcon from '../icons/JsonConverter.png';
 
 export class NavMenu extends Component {
     static displayName = NavMenu.name;
